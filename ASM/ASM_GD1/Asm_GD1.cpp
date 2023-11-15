d7bdf7b6a0e515cf6cdc573d406c43155a61c0ee
ngynetahnuv <nguyenthanhvudvfb@gmail.com>
2023-11-15 09:23:38 +0700

    update

--- ASM/ASM_GD1/Asm_GD1.cpp
+++ ASM/ASM_GD1/Asm_GD1.cpp
@@ -4,12 +4,20 @@ using namespace std;
 
 int main () {
     int luachon;
-    cout << "-----------------------------" << endl;
-    cout << "1. " << endl;
-    cout << "2. " << endl;
-    cout << "3. " << endl;
-    cout << "4. Thoat" << endl;
-    cout << "Vui long lua chon: "; cin >> luachon;
+        cout << "----------------- Menu chuc nang ---------------\n";
+        cout << "1. Kiem tra so nguyen\n";
+        cout << "2. Tim uoc chung boi chung cua 2 so\n";
+        cout << "3. Tinh tien cho quan Karaoke\n";
+        cout << "4. Tinh tien dien\n";
+        cout << "5. Chuc nang doi tien\n";
+        cout << "6. Tinh lai suat ngan hang vay tra gop\n";
+        cout << "7. Chuong trinh vay tien mua xe\n";
+        cout << "8. Thong tin sinh vien\n";
+        cout << "9. Game FPOLY-LOTT\n";
+        cout << "10. Chuong trinh tinh toan phan so\n";
+        cout << "11. Thoat\n";
+        cout << "--------------------------------------\n";
+        cout << "Vui long lua chon: "; cin >> luachon;
     // dung vong lap do while de thoat khoi chuong trinh neu nhap khac so trong mennu
     do {
         switch (luachon)
@@ -27,6 +35,6 @@ int main () {
             cout << "thoat chuong trinh" << endl;
             break;
         }
-    } while ( luachon != 4 );
+    } while ( luachon != 11 );
     return 0;
 }
